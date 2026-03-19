2fb38dc71b8761b6525b0540dda9d51299d59c1e
Marcos Higildo <marcosmuangula@gmail.com>
2026-03-19 22:41:12 +0200

    feat: create responsive hero section with costomized local copywriting closes #7

--- frontend/ndlela-front/src/app/page.tsx
+++ frontend/ndlela-front/src/app/page.tsx
@@ -1,11 +1,12 @@
 import Navbar from "../components/landing/Navbar";
+import Hero from "../components/landing/Hero";
 
 export default function Page() {
   return (
     <div className="">
       <section>
         <Navbar/>
-
+        <Hero/>
       </section>
     </div>
   );
