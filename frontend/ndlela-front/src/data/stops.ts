29dfcaa570994a06b3e50448cea662842f2e3b4e
Marcos Higildo <marcosmuangula@gmail.com>
2026-04-16 23:50:00 +0200

    feat(data): add initial stops dataset with real coordinates and metadata

---
+++ frontend/ndlela-front/src/data/stops.ts
@@ -0,0 +1,299 @@
+import { Stop } from "../types/stops";
+
+export const stops: Record<string, Stop> = {
+ "machava-socimol": {
+    id: "machava-socimol",
+    name: "Machava Socimol",
+    coordinates: { lat: -25.883030738292288, lng: 32.47732025148288 },
+    isOfficial: true,
+    description: "Fabrica Merec, Av.Josina Machel, Matola",
+    aliases: ["Socimol", "Merec", "Home Store"],
+    landmark: "Socimol",
+  },
+
+ "muchina":  {
+    id: "muchina",
+    name: "Muchina",
+    coordinates: { lat: -25.887838272286505, lng: 32.47952988021427 },
+    isOfficial: true,
+    description: "Armazens de Arroz, Av.Josina Machel",
+    aliases: ["Muxhina"],
+    landmark: "Muchina",
+  },
+
+  "baião": {
+    id: "baião",
+    name: "Baião",
+    coordinates: { lat: -25.89173255809368, lng: 32.48135738074155 },
+    isOfficial: true,
+    description: "Baião, Av.Josina Machel, Matola",
+    aliases: ["Baião"],
+    landmark: "Posto policial",
+  },
+
+  "pinheiros": {
+    id: "pinheiros",
+    name: "Pinheiros",
+    coordinates: { lat: -25.898407020475002, lng: 32.48357022974732 },
+    isOfficial: true,
+    description: "Pinheiros, Av.Josina Machel, Matola",
+    aliases: ["Pinheiros"],
+    landmark: "pinheiros",
+  },
+
+ "mafureira": {
+    id: "mafureira",
+    name: "Mafureira",
+    coordinates: { lat: -25.90408129098925, lng: 32.48350571896851 },
+    isOfficial: true,
+    description: "Cruzamento da Mafureira, Av.Josina Machel, Matola",
+    aliases: ["Mafureira"],
+    landmark: "Mafureira",
+  },
+
+ "romos": {
+    id: "romos",
+    name: "Romos",
+    coordinates: { lat: -25.906494302293783, lng: 32.487648058722726 },
+    isOfficial: true,
+    description:
+      "Romos, Estação de Serviços Bela Isabel, Av.Josina Macheal, Matola",
+    aliases: ["Romos"],
+    landmark: "Romos",
+  },
+
+  "naz-naz": {
+    id: "naz-naz",
+    name: "Naz-Naz",
+    coordinates: { lat: -25.908833854217434, lng: 32.49139505976038 },
+    isOfficial: true,
+    description:
+      "Naz-Naz, Rua do mercado da machava, Av.Josina Macheal, Matola ",
+    aliases: ["Naz-Naz"],
+    landmark: "Naz-Naz",
+  },
+
+  "direccao": {
+    id: "direccao",
+    name: "Direcção",
+    coordinates: {lat: -25.910228842640677, lng: 32.49404637855647},
+    isOfficial: true,
+    description: "Direcção Provinvial De Trabalho, Av.Josina Macheal, Matola",
+    aliases: ["Direcção"],
+    landmark: "Direcção"
+  },
+
+  "machava-sede": {
+    id: "machava-sede",
+    name: "Machava Sede",
+    coordinates: { lat: -25.91142602719016, lng: 32.496113137594534 },
+    isOfficial: true,
+    description:
+      "Machava-sede, Bombas galp, Escola Secundária da Machava Sede, Av.Josina Macheal, Matola",
+    aliases: ["Bombas", "machava-sede"],
+    landmark: "Machava Sede",
+  },
+
+  "tamega": {
+    id: "tamega",
+    name: "Tâmega",
+    coordinates: { lat: -25.916355909913687, lng: 32.50483059287187 },
+    isOfficial: true,
+    description: "Tâmega,Av.Josina Macheal, Matola",
+    aliases: ["Tâmega"],
+    landmark: "Tâmega",
+  },
+
+  "coca-cola": {
+    id: "coca-cola",
+    name: "Coca-Cola",
+    coordinates: { lat: -25.919128408010604, lng: 32.50968678978744 },
+    isOfficial: true,
+    description:
+      "Coca-Cola, Bombas Engen coca cola, Cruzamento entre as Avinidas Josina Macheal e Eduardo Mondlane",
+    aliases: ["Coca Cola"],
+    landmark: "coca cola",
+  },
+
+  "bananeiras": {
+    id: "bananeiras",
+    name: "Bananeiras",
+    coordinates: { lat: -25.926790544346023, lng: 32.50538663793221 },
+    isOfficial: true,
+    description:
+      "Bananeiras, cruzamento entre as Av.Eduardo Mondlane e Av.Das Indústrias",
+    aliases: ["Bananeiras"],
+    landmark: "Bananeiras",
+  },
+
+  "casa-branca": {
+    id: "casa-branca",
+    name: "Casa Branca",
+    coordinates: { lat: -25.933292162163102, lng: 32.50338232729118 },
+    isOfficial: true,
+    description: "Casa Branca, Trevo, N4",
+    aliases: ["casa branca"],
+    landmark: "casa branca",
+  },
+
+  "CMC": {
+    id: "CMC",
+    name: "CMC",
+    coordinates: { lat: -25.942596494745583, lng: 32.48364304057414 },
+    isOfficial: false,
+    description: "CMC, Fomento, N4",
+    aliases: ["CMC"],
+    landmark: "CMC",
+  },
+
+  "Pilvi": {
+    id: "pilvi",
+    name: "Pilvi",
+    coordinates: { lat: -25.945445666552008, lng: 32.47878005983945 },
+    isOfficial: true,
+    description: "Pilvi, N4",
+    aliases: ["Pilvi"],
+    landmark: "Pilvi",
+  },
+
+  "bic": {
+    id: "bic",
+    name: "Bic",
+    coordinates: { lat: -25.949762409562766, lng: 32.469991391463374 },
+    isOfficial: true,
+    description: "Bic, N4",
+    aliases: ["Bic"],
+    landmark: "Bic",
+  },
+
+  "mangueira": {
+    id: "magueira",
+    name: "Mangueira",
+    coordinates: { lat: -25.95230974794953, lng: 32.46414004657206 },
+    isOfficial: true,
+    description: "Mangueiras, Maeva Plast, N4",
+    aliases: ["Mangueiras"],
+    landmark: "Mngueiras",
+  },
+
+  "ceres": {
+    id: "ceres",
+    name: "Ceres",
+    coordinates: { lat: -25.954150700886213, lng: 32.45985482958998 },
+    isOfficial: true,
+    description: "Ceres, Shoprite (Matola), N2",
+    aliases: ["Ceres"],
+    landmark: "Ceres",
+  },
+
+  "joao-mateus": {
+    id: "joao-mateus",
+    name: "João Mateus",
+    coordinates: { lat: -25.957667419296342, lng: 32.45249396190779 },
+    isOfficial: true,
+    description: "João Mateus, N2, Matola",
+    aliases: ["João Mateus"],
+    landmark: "João Mateus",
+  },
+
+  "hospital": {
+    id: "hospital",
+    name: "Hospital",
+    coordinates: { lat: -25.967274310862702, lng: 32.44416951090982 },
+    isOfficial: true,
+    description: "Hospital, Hospital Provincial da Matola",
+    aliases: ["Hospital"],
+    landmark: "Hospital",
+  },
+
+  "matola-rio": {
+    id: "matola-rio",
+    name: "Matola Rio",
+    coordinates: { lat: -25.979138969542113, lng: 32.43423039056809 },
+    isOfficial: true,
+    description: "Matola rio, N2 Matola",
+    aliases: ["matola rio"],
+    landmark: "Matola Rio",
+  },
+
+  "alianca": {
+    id: "alianca",
+    name: "Aliança",
+    coordinates: { lat: -25.955748191628096, lng: 32.41331344175703 },
+    isOfficial: true,
+    description: "Paragem Aliança, Complexo Aliança, Rua da Mozal, Matola Rio",
+    aliases: ["Complexo Aliança"],
+    landmark: "Aliança",
+  },
+
+  "antena": {
+    id: "antena",
+    name: "Antena",
+    coordinates: { lat: -25.950063910631847, lng: 32.41067258100669 },
+    isOfficial: true,
+    description: "antena, rua da Mozal, Matola Rio",
+    aliases: ["antenas"],
+    landmark: "Antena",
+  },
+
+  "cruzamento": {
+    id: "cruzamento",
+    name: "Cruzamento",
+    coordinates: { lat: -25.945931774301233, lng: 32.40875390729603 },
+    isOfficial: true,
+    description: "Cruzamento, Rua da mozal, Matola Rio",
+    aliases: ["Cruzamneto"],
+    landmark: "Cruzamento",
+  },
+
+  "mandela": {
+    id: "mandela",
+    name: "Mandela",
+    coordinates: { lat: -25.939616483133317, lng: 32.40576937791867 },
+    isOfficial: true,
+    description: "Escola Secundária Nelson Mandela, Rua da Mozal, Matola Rio",
+    aliases: ["Escola Mandela"],
+    landmark: "Escola Mandela",
+  },
+
+  "instituto": {
+    id: "instituto-guebuza",
+    name: "Instituto Armando Guebuza",
+    coordinates: { lat: -25.93730338656528, lng: 32.404749554991206 },
+    isOfficial: true,
+    description:
+      "INSTITUTO INDUSTRIAL E DE COMPUTAÇÃO Armando Emílio Guebuza, Bairro de Djuba, Rua da Mozal, Matola Rio.",
+    aliases: ["Instituto"],
+    landmark: "Instituto",
+  },
+
+  "cajueiro": {
+    id: "cajueiro",
+    name: "Cajueiro",
+    coordinates: { lat: -25.936508710957934, lng: 32.404363316903755 },
+    isOfficial: true,
+    description: "Paragem Cajueiro, Bairro de Djuba, Rua da Mozal, Matola Rio.",
+    aliases: ["Cajueiro"],
+    landmark: "Cajueiro",
+  },
+
+  "primeira-entrada": {
+    id: "primeira-entrada",
+    name: "Primeira Entrada",
+    coordinates: { lat: -25.92566852719467, lng: 32.39933789279856 },
+    isOfficial: true,
+    description: "Primeira Entrada, Beluluane, Rua da Mozal, Matola Rio",
+    aliases: ["Primeira entrada"],
+    landmark: "Primeira entrada",
+  },
+
+  "mozal": {
+    id: "mozal",
+    name: "Mozal",
+    coordinates: { lat: -25.91577190852332, lng: 32.3946037349799 },
+    isOfficial: true,
+    description: "Terminal da Mozal",
+    aliases: ["Mozal"],
+    landmark: "Mozal",
+  },
+};
